e42862c2479c7ebd29e96b30397ad3551e239514
Vimal Soni <vimal.h.soni@gmail.com>
2019-04-03 03:04:27 +0000

    Automatically backed up by Learn

--- spec/bundler_spec.rb
+++ spec/bundler_spec.rb
@@ -63,7 +63,7 @@ describe "Bundler" do
       it "should contain the rspec gem in the test group using block syntax" do
         expect(@gemfile_text =~ /group (:test|['"]test['"]) do/).not_to eq(nil)
         expect(@bundle_output =~ /rspec/).not_to eq(nil)
-  
+
         bundle_output_without_test = ""
         Bundler.with_clean_env do
           bundle_output_without_test = `bundle --without test`
